fbf508db2f75758f74d20f4ab60de3d32404a9d6
aniketnegii <aniket.negi2112@gmail.com>
2021-09-28 00:10:08 +0530

    first commit

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '07b70a0cfemsh7e92788289e4639p1e6117jsn5c2a7912b061'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ( { url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath : 'cryptoApi',
+    baseQuery : fetchBaseQuery( { baseUrl }),
+    endpoints : (builder) => ({
+        getCryptos : builder.query({
+            query : () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
+  
