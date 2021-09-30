3ef08c037e6e584fd34795f8221a87e7e7ee1dda
aniketnegii <aniket.negi2112@gmail.com>
2021-09-30 21:30:10 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,18 +48,20 @@ const Navbar = () => {
             {
                 activeMenu && (
                     <Menu theme="dark" >
-                        <Menu.Item icon={<HomeOutlined/>}>
-                            <Link to="/">Home</Link>
-                        </Menu.Item>
-                        <Menu.Item icon={<FundOutlined/>}>
-                            <Link to="/cryptocurrencies"> CryptoCurrencies </Link>
-                        </Menu.Item>
-                        <Menu.Item icon={<MoneyCollectOutlined/>}>
-                            <Link to="/exchanges">Exchanges</Link>
-                        </Menu.Item>
-                        <Menu.Item icon={<BulbOutlined/>}>
-                            <Link to="/news"> News </Link>
-                        </Menu.Item>             
+                        <div onClick={() => setActiveMenu(!activeMenu)} >
+                            <Menu.Item icon={<HomeOutlined/>}>
+                                <Link to="/">Home</Link>
+                            </Menu.Item>
+                            <Menu.Item icon={<FundOutlined/>}>
+                                <Link to="/cryptocurrencies"> CryptoCurrencies </Link>
+                            </Menu.Item>
+                            <Menu.Item icon={<MoneyCollectOutlined/>}>
+                                <Link to="/exchanges">Exchanges</Link>
+                            </Menu.Item>
+                            <Menu.Item icon={<BulbOutlined/>}>
+                                <Link to="/news"> News </Link>
+                            </Menu.Item>
+                        </div>             
                     </Menu>       
                 )
             }
